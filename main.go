c87e15d75b9a92738f8b28563ee00c6b325e5a19
orensimple <orensimple@gmail.com>
2019-09-19 12:17:30 +0300

    v 1.01

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"unicode"
 )
 
 func main() {
@@ -16,45 +18,33 @@ func main() {
 }
 
 func stringConvert(text string) string {
-	_, error := strconv.Atoi(string(text[0]))
-	if error == nil {
+	if unicode.IsDigit(rune(text[0])) {
 		return ""
-	} else {
-		var newText, symbolBuf string
-		var slashBuf bool
-		for _, runeValue := range text {
-			symbol := string(runeValue)
-			i, error := strconv.Atoi(symbol)
-			if error != nil {
-				if symbol != `\` {
-					newText = newText + symbol
-				} else if slashBuf == true {
-					newText = newText + symbol
-				}
-			} else {
-				if i > 0 {
-					if slashBuf != true {
-						for a := 2; a <= i; a++ {
-							newText = newText + symbolBuf
-						}
-					}
-				}
-				if slashBuf == true {
-					newText = newText + symbol
-				}
-
+	}
+	var symbolBuf string
+	var newText strings.Builder
+	var slashBuf bool
+	for _, runeValue := range text {
+		symbol := string(runeValue)
+		if !unicode.IsDigit(runeValue) {
+			if symbol != `\` || slashBuf {
+				newText.WriteString(symbol)
 			}
-			symbolBuf = symbol
-			if symbol == `\` {
-				if slashBuf == true {
-					slashBuf = false
-				} else {
-					slashBuf = true
-				}
-			} else {
-				slashBuf = false
+		} else {
+			count, _ := strconv.Atoi(symbol)
+			if count > 0 && !slashBuf {
+				newText.WriteString(strings.Repeat(symbolBuf, count-1))
+			}
+			if slashBuf {
+				newText.WriteString(symbol)
 			}
 		}
-		return newText
+		symbolBuf = symbol
+		if symbol == `\` && !slashBuf {
+			slashBuf = true
+		} else {
+			slashBuf = false
+		}
 	}
+	return newText.String()
 }
--- main_test.go
+++ main_test.go
@@ -4,37 +4,26 @@ import (
 	"testing"
 )
 
-const testResult1 = `aaaabccddddde`
-const testResult2 = `abcd`
-const testResult3 = ``
-const testResult4 = `qwe45`
-const testResult5 = `qwe44444`
-const testResult6 = `qwe\\\\\`
+var flagtests = []struct {
+	in  string
+	out string
+}{
+	{"a4bc2d5e", "aaaabccddddde"},
+	{"abcd", "abcd"},
+	{"45", ""},
+	{`qwe\4\5`, "qwe45"},
+	{`qwe\45`, "qwe44444"},
+	{`qwe\\5`, `qwe\\\\\`},
+}
 
 func TestStringConvert(t *testing.T) {
-	result := stringConvert("a4bc2d5e")
-	if result != testResult1 {
-		t.Errorf("%s %s", result, testResult1)
-	}
-	result = stringConvert("abcd")
-	if result != testResult2 {
-		t.Errorf("%s %s", result, testResult2)
-	}
-	result = stringConvert("45")
-	if result != testResult3 {
-		t.Errorf("%s %s", result, testResult4)
-	}
-	result = stringConvert(`qwe\4\5`)
-	if result != testResult4 {
-		t.Errorf("%s %s", result, testResult4)
-	}
-	result = stringConvert(`qwe\45`)
-	if result != testResult5 {
-		t.Errorf("%s %s", result, testResult5)
-	}
-	result = stringConvert(`qwe\\5`)
-	if result != testResult6 {
-		t.Errorf("%s %s", result, testResult6)
+	for _, tt := range flagtests {
+		t.Run(tt.in, func(t *testing.T) {
+			s := stringConvert(tt.in)
+			if s != tt.out {
+				t.Errorf("got %q, want %q", s, tt.out)
+			}
+		})
 	}
 
 }
